61b60991a11fbf29d95f11bb760a600ff871235a
WillianWCR <willian@willianwcr.com.br>
2020-03-20 12:45:48 -0300

    Mobile Menu fixed

--- main.js
+++ main.js
@@ -7,3 +7,30 @@ window.onscroll = function() {
         header.classList.remove("scrolled");
     }
 };
+
+var collapseElements = document.getElementsByClassName("collapse");
+var i;
+
+for (i = 0; i < collapseElements.length; i++) {
+    collapseElements[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.querySelector('.dropdown');
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+    });
+}
+
+document.getElementById("menu-hamburguer").addEventListener("click", function() {
+    var menu = document.getElementsByClassName("menu")[0];
+    menu.classList.toggle("active");
+    if (menu.style.maxHeight){
+        menu.style.maxHeight = null;
+        document.getElementById("main-header").style.backgroundColor = "";
+    } else {
+        menu.style.maxHeight = "100vh";
+        document.getElementById("main-header").style.backgroundColor = "#fff";
+      }
+});
